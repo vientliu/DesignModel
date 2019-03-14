4f318efbd0e94b137de12a564f3f4199927f96f8
tang <1220323287@qq.com>
2019-03-14 16:52:39 +0800

    java自带的观察者模式

--- src/cn/blackfish/designofjava/Oserver1.java
+++ src/cn/blackfish/designofjava/Oserver1.java
@@ -15,6 +15,7 @@ public class Oserver1 implements Observer {
     public void update(Observable o, Object arg) {
         if (o instanceof SubjectFor3D)
         {
+            // 这是java自带的观察者模式
             SubjectFor3D subjectFor3d = (SubjectFor3D) o;
             System.out.println("subjectFor3d's msg -- >" + subjectFor3d.getMsg());
         }
